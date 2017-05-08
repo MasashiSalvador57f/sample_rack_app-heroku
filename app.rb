575e168544973ed6f8a901f88b6ffd93b1903785
Masashi Salvador Mitsuzawa <masashisalvador57f@gmail.com>
2017-05-08 13:29:56 +0900

    fix

--- app.rb
+++ app.rb
@@ -5,15 +5,8 @@ ADMIN_SERVER_PORT = 19899
 class SimpleApp
   def call(env)
     request = Rack::Request.new(env)
-
-    request_uri = env['REQUEST_URI']
     request_path = env['REQUEST_PATH']
 
-    p "====="
-    p request_uri
-    p request_path
-    p request_path.start_with?(ADMIN_PATH)
-    p "====="
     if request_path.start_with?(ADMIN_PATH)
       [ 301, {'Location' => "http://localhost:#{ADMIN_SERVER_PORT}#{request_path}" }, self ]
     else
